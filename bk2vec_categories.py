675e78d1487f0336916c38e58a87298e88e06486
Yaroslav Nechaev <remper@me.com>
2016-04-15 18:26:10 +0200

    At least this one works

--- bk2vec_categories.py
+++ bk2vec_categories.py
@@ -21,7 +21,7 @@ csv.field_size_limit(2147483647)
 def wordreader(filename):
   previous = list()
   with gzip.open(filename, 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter='\t')
+    reader = csv.reader(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE, quotechar='')
     try:
       for row in reader:
         if (len(row) is not 2):
@@ -54,7 +54,7 @@ def build_dictionary(reader):
   return dictionary, reverse_dictionary
 
 # Limiter if there are performance issues
-max_pages = 0
+max_pages = 500
 max_categories_per_page = 0
 
 def build_pages(filename, dictionary):
@@ -67,7 +67,7 @@ def build_pages(filename, dictionary):
   category_found = 0
   category_notfound = 0
   with gzip.open(filename, 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter='\t')
+    reader = csv.reader(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE, quotechar='')
     try:
       for row in reader:
         page_title = row[0]
@@ -107,7 +107,7 @@ def restore_dictionary(filename):
   reverse_dictionary = dict()
   processed = 0
   with open(filename + "_dict", 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter='\t')
+    reader = csv.reader(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE, quotechar='')
     for row in reader:
       row[0] = str(row[0])
       row[1] = int(row[1])
@@ -164,9 +164,13 @@ def generate_batch(reader, dictionary, batch_size, num_skips, skip_window):
       targets_to_avoid.append(target)
       if buffer[skip_window] not in dictionary:
         print("Word", buffer[skip_window], "is not in dictionary")
+        print(buffer)
+        print(dictionary[56])
         exit()
       if buffer[target] not in dictionary:
         print("Word", buffer[target], "is not in dictionary")
+        print(dictionary[56])
+        print(buffer)
         exit()
       batch[i * num_skips + j] = dictionary[buffer[skip_window]]
       labels[i * num_skips + j, 0] = dictionary[buffer[target]]
@@ -181,15 +185,15 @@ for i in range(8):
   print(reverse_dictionary[batch[i]], '->', reverse_dictionary[labels[i, 0]])
 # Step 4: Build and train a skip-gram model.
 
-batch_size = 64
-embedding_size = 64  # Dimension of the embedding vector.
+batch_size = 128
+embedding_size = 16  # Dimension of the embedding vector.
 skip_window = 1       # How many words to consider left and right.
 num_skips = 2         # How many times to reuse an input to generate a label.
 
 # We pick a random validation set to sample nearest neighbors. Here we limit the
 # validation samples to the words that have a low numeric ID, which by
 # construction are also the most frequent.
-valid_size = 16     # Random set of words to evaluate similarity on.
+valid_size = 8     # Random set of words to evaluate similarity on.
 valid_window = 100  # Only pick dev samples in the head of the distribution.
 valid_examples = np.array(random.sample([x for x in range(valid_window)], valid_size))
 num_sampled = 64    # Number of negative examples to sample.
@@ -209,63 +213,57 @@ graph = tf.Graph()
 with graph.as_default():
 
   # Input data.
-  train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
-  train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+  train_inputs = tf.placeholder(tf.int32, shape=[batch_size], name="train_inputs")
+  train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1], name="train_labels")
+  train_categories = tf.placeholder(tf.int32, shape=[None], name="train_categories")
+  train_category_indexes = tf.placeholder(tf.int32, shape=[None], name="train_category_indexes")
 
   valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
 
   # Allocating categorial knowledge on a GPU
-  with tf.device('/cpu:0'):
+  #with tf.device('/cpu:0'):
 
     # Generating a boolean mask for page categories
     # category_mask shape is vocabulary_size x vocabulary_size
 
     # pages = dict() page index -> category indices
-    print("Building indices")
-    value_count = 0
-    pages_parsed = 0
-    true_indices = list()
-    for page in pages.keys():
-      value_count += len(pages[page])
-      pages_parsed += 1
-      for i in pages[page]:
-        true_indices.append(tf.constant([page, i]))
-      if pages_parsed % 50000 == 0:
-        print("  " + str(pages_parsed // 10000) + "0k pages converted")
-    category_mask = tf.SparseTensor(indices=true_indices, values=tf.constant(True, shape=[value_count]),
-                                    shape=[vocabulary_size, vocabulary_size])
-    del true_indices
-    del pages
-    print("Indices built")
+    #print("Building indices")
+    #value_count = 0
+    #pages_parsed = 0
+    #true_indices = list()
+    #for page in pages.keys():
+    #  value_count += len(pages[page])
+    #  pages_parsed += 1
+    # for i in pages[page]:
+    #    true_indices.append([page, i])
+    #  if pages_parsed % 1000000 == 0:
+    #    print("  " + str(pages_parsed // 1000000) + "m pages converted")
+    #category_mask = tf.SparseTensor(indices=true_indices, values=[True]*value_count,
+    #                                shape=[vocabulary_size, vocabulary_size])
+
+    #del true_indices
+    #del pages
+    #print("Indices built")
 
   # Ops and variables pinned to the CPU because of missing GPU implementation
   with tf.device('/cpu:0'):
     # Look up embeddings for inputs.
     embeddings = tf.Variable(
-        tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+        tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0), name="embeddings")
     embed = tf.nn.embedding_lookup(embeddings, train_inputs)
 
     # Construct the variables for the NCE loss
     nce_weights = tf.Variable(
         tf.truncated_normal([vocabulary_size, embedding_size],
-                            stddev=1.0 / math.sqrt(embedding_size)))
-    nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+                            stddev=1.0 / math.sqrt(embedding_size)), name="NCE_weights")
+    nce_biases = tf.Variable(tf.zeros([vocabulary_size]), name="NCE_biases")
 
   # Recalculating centroids for words in a batch
-  with tf.name_scope("category_centroids"):
-    # Zero-vector for comparison purposes
-    zeros = tf.zeros(embedding_size)
-
-    # Selecting masks that are relevant to a curent batch
-    current_batch_category_masks = tf.unpack(tf.nn.embedding_lookup(category_mask, train_inputs))
-
-    # Recalculating centroids for each selected word
-    recalculated_centroids = tf.nn.embedding_lookup(embeddings, train_inputs)
-    for id, mask in enumerate(current_batch_category_masks):
-      centroid = centroid(tf.boolean_mask(embeddings, mask))
-      recalculated_centroids[id] = tf.cond(tf.equal(centroid, zeros), recalculated_centroids[id], centroid)
-    recalculated_centroids = tf.pack(recalculated_centroids, "batch_centroids")
-
+  with tf.name_scope("category_distances"):
+    category_distances = matrix_distance(
+      tf.gather(embeddings, train_categories),
+      tf.gather(embeddings, train_category_indexes)
+    )
 
   # Precomputed centroids for each embeddings vector
   # Initialize them with embeddings by default
@@ -280,7 +278,7 @@ with graph.as_default():
   # Categorical knowledge additional term
   with tf.name_scope("category_loss"):
     # Building category objective which is average distance to word centroid
-    category_loss = tf.reduce_mean(matrix_distance(embed, recalculated_centroids))
+    category_loss = tf.reduce_mean(category_distances)
     category_loss_summary = tf.scalar_summary("category_loss", category_loss)
 
   # Compute the average NCE loss for the batch.
@@ -313,16 +311,29 @@ num_steps = 100001
 with tf.Session(graph=graph) as session:
   # merged = tf.merge_all_summaries()
   category_merged = tf.merge_summary([skipgram_loss_summary, loss_summary, category_loss_summary])
-  writer = tf.train.SummaryWriter("logs", session.graph_def)
-  # We must initialize all variables before we use them.
+  writer = tf.train.SummaryWriter("logs", graph)
   tf.initialize_all_variables().run()
   print("Initialized")
 
   average_loss = 0
-  average_loss2 = 0
+  average_cat_per_page = 0
   for step in xrange(num_steps):
     batch_inputs, batch_labels = generate_batch(data_reader, dictionary, batch_size, num_skips, skip_window)
-    feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+    categories = list()
+    category_indexes = list()
+    for id, input in enumerate(batch_inputs):
+      if input in pages:
+        for i in pages[input]:
+          category_indexes.append(id)
+          categories.append(i)
+      else:
+        category_indexes.append(id)
+        categories.append(input)
+    average_cat_per_page += len(categories)
+    feed_dict = {
+      train_inputs: batch_inputs, train_labels: batch_labels,
+      train_categories: categories, train_category_indexes: category_indexes
+    }
 
     # We perform one update step by evaluating the optimizer op (including it
     # in the list of returned values for session.run()
@@ -339,10 +350,12 @@ with tf.Session(graph=graph) as session:
     if step % 2000 == 0:
       if step > 0:
         average_loss /= 2000
+        average_cat_per_page /= 2000
       # The average loss is an estimate of the loss over the last 2000 batches.
       print("Average loss at step ", step, ": ", average_loss)
-      # print("Average category loss at step ", step, ": ", average_loss2)
+      print("Average categories per batch:", average_cat_per_page)
       average_loss = 0
+      average_cat_per_page = 0
 
     # Note that this is expensive (~20% slowdown if computed every 500 steps)
     if step % 10000 == 0:
@@ -351,11 +364,11 @@ with tf.Session(graph=graph) as session:
         valid_word = reverse_dictionary[valid_examples[i]]
         top_k = 8 # number of nearest neighbors
         nearest = (-sim[i, :]).argsort()[1:top_k+1]
-        log_str = u"Nearest to %s:" % valid_word
+        log_str = "Nearest to %s:" % valid_word
         for k in xrange(top_k):
           close_word = reverse_dictionary[nearest[k]]
-          log_str = u"%s %s," % (log_str, close_word)
-        print(log_str.encode('utf-8'))
+          log_str = "%s %s," % (log_str, close_word)
+        print(log_str)
   final_embeddings = normalized_embeddings.eval()
 
 # Step 6: Dump embeddings to file
