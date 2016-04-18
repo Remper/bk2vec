38e465ca27f3c745ef5fefd6af172bd5a5dbc5b8
Yaroslav Nechaev <remper@me.com>
2016-04-18 17:21:49 +0200

    I'm an idiot

--- evaluation.py
+++ evaluation.py
@@ -5,35 +5,16 @@ from __future__ import print_function
 import gzip
 import csv
 import tensorflow as tf
+import numpy as np
 
-#DICTIONARY = '/Users/remper/Projects/bk2vec/borean/embeddings_80_500k_traintest_dict.gz'
 #CATEGORIES = '/Users/remper/Projects/bk2vec/borean/embeddings_80_500k_traintest_test.gz'
 #EMBEDDINGS = '/Users/remper/Projects/bk2vec/borean/embeddings_80_500k_traintest.tsv.gz'
 # Borean
-#DICTIONARY = '/borean1/data/pokedem-experiments/bk2vec/alessio/enwiki-20140203-text-cleaned.csv.gz_dict'
 #CATEGORIES = '/borean1/data/pokedem-experiments/bk2vec/embeddings_80_500k_traintest_test.gz'
 #EMBEDDINGS = '/borean1/data/pokedem-experiments/bk2vec/embeddings_80_500k_traintest.tsv.gz'
 # Auster
-DICTIONARY = 'embeddings_80_500k_traintest_dict.gz'
 CATEGORIES = 'embeddings_80_500k_traintest_test.gz'
 EMBEDDINGS = 'embeddings_80_500k_traintest.tsv.gz'
-
-def restore_dictionary(filename):
-  dictionary = dict()
-  reverse_dictionary = dict()
-  processed = 0
-  with gzip.open(filename, 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE, quotechar='')
-    for row in reader:
-      row[0] = str(row[0])
-      row[1] = int(row[1])
-      processed += 1
-      if processed % 3000000 == 0:
-        print("  " + str(processed // 1000000) + "m words parsed")
-      dictionary[row[0]] = row[1]
-      reverse_dictionary[row[1]] = row[0]
-  return dictionary, reverse_dictionary
-
 
 def restore_evaluation(filename):
   pages = dict()
@@ -41,16 +22,14 @@ def restore_evaluation(filename):
   with gzip.open(filename, 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE, quotechar='')
     for row in reader:
-      if row[0] not in pages:
-        pages[row[0]] = list()
-      categories = pages[row[0]]
-      categories.extend(row[1:])
-      categories += len(row[1:])
+      index = int(row[0])
+      pages[index] = np.array(map(int, row[1:]))
+      categories += len(pages[index])
   print("Loaded", len(pages.values()), "pages with", categories, "categories")
   return pages
 
 
-def restore_embeddings(dictionary, filename):
+def restore_embeddings(filename):
   embeddings = list()
   embeddings_size = 0
   embeddings_count = 0
@@ -60,15 +39,8 @@ def restore_embeddings(dictionary, filename):
       for row in reader:
         if embeddings_size == 0:
           embeddings_size = len(row) - 1
-        if row[0] not in dictionary:
-          print("Word", row[0], "haven't been found in a dictionary")
-          continue
-        dict_index = dictionary[row[0]]
-        if dict_index != len(embeddings):
-          print("Inconsistency dictionary -> embeddings, pages might be off", dict_index, len(embeddings))
-          exit()
         embeddings_count += 1
-        embedding = map(float, row[1:])
+        embedding = np.array(map(float, row[1:]))
         embeddings.append(embedding)
         if embeddings_count % 1000000 == 0:
           print("  "+str(embeddings_count // 1000000)+"m words parsed")
@@ -76,15 +48,10 @@ def restore_embeddings(dictionary, filename):
       print(u"Dunno why this error happens")
   return embeddings, embeddings_size
 
-
-print("Restoring dictionary")
-dictionary, reverse_dictionary = restore_dictionary(DICTIONARY)
-print("Dictionary restored")
 print("Restoring embeddings")
-embeddings, embeddings_size = restore_embeddings(dictionary, EMBEDDINGS)
+embeddings, embeddings_size = restore_embeddings(EMBEDDINGS)
 pages = restore_evaluation(CATEGORIES)
 print(len(embeddings), "loaded of", embeddings_size, "dimensions")
-print("Sample embedding: yaroslav", embeddings[dictionary['yaroslav']])
 
 graph = tf.Graph()
 
