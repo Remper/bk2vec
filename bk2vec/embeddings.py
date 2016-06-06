Limits on the batch size, new evaluation, fixes to embeddings and relations, fixes to normalization

bk2vec/embeddings.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import gzip
 import numpy as np
 import time
-
+from bk2vec.dictionary import Dictionary
 
 class Embeddings:
     def __init__(self, vocabulary_size, embeddings_size):
@@ -25,12 +25,12 @@ class Embeddings:
     def normalized_tensor(self):
         if self._norm_tensor is None:
             with tf.name_scope("embeddings_normalization"):
-                norm = tf.sqrt(tf.reduce_sum(tf.square(self.tensor()), 1, keep_dims=True))
+                norm = tf.sqrt(tf.reduce_sum(tf.square(self.tensor())))
                 self._norm_tensor = tf.div(self.tensor(), norm)
         return self._norm_tensor
 
     def dump(self, filename, norm_tensor, dictionary):
-        with open(filename, 'wb') as writer:
+        with gzip.open(filename, 'wb') as writer:
             counter = 0
             timestamp = time.time()
             for idx, vector in enumerate(norm_tensor):
@@ -38,7 +38,7 @@ class Embeddings:
                 if idx not in dictionary.rev_dict:
                     print("Bullshit happened: ", idx, vector)
                     return
-                if counter % 2000000 == 0:
+                if counter % 1000000 == 0:
                     print("  ", str(counter // 1000000) + "m writes (" + ("%.5f" % (time.time() - timestamp)) + "s)")
                     timestamp = time.time()
                 writer.write(dictionary.rev_dict[idx])
@@ -51,16 +51,16 @@ class Embeddings:
         final_embeddings = list()
         embeddings = list()
         count = 0
-        rev_dict = dict()
-        dictionary = dict()
+        dictionary = Dictionary()
         with gzip.open(filename, 'rb') as reader:
             timestamp = time.time()
             for line in reader:
                 row = line.split('\t')
                 #embeddings.append(np.array(map(float, row[1:])))
-                embeddings.append(np.array([float(ele.strip().strip('"')) for ele in row[1:]]))
-                rev_dict[count] = row[0]
-                dictionary[row[0]] = count
+                if len(embeddings) > 0 and len(row)-1 != embeddings[0].shape[0]:
+                    continue
+                embeddings.append(np.array([float(ele.strip()) for ele in row[1:]]))
+                dictionary.put_word(count, row[0])
                 count += 1
                 if count % 2000000 == 0:
                     print("  ", str(count // 1000000) + "m words parsed (" + ("%.5f" % (time.time() - timestamp)) + "s)")
@@ -70,6 +70,6 @@ class Embeddings:
                     embeddings = list()
                     print("  Squashed")
         print("  ", str(count // 1000000) + "m words parsed (" + ("%.5f" % (time.time() - timestamp)) + "s)")
-        final_embeddings.append(np.array(embeddings))
+        final_embeddings.append(np.vstack(embeddings))
         del embeddings
-        return np.vstack(final_embeddings), rev_dict, dictionary
+        return np.vstack(final_embeddings), dictionary

bk2vec/evaluation.py
@@ -9,6 +9,7 @@ from scipy.spatial.distance import cdist
 
 import gzip
 import csv
+import time
 import numpy as np
 import tensorflow as tf
 
@@ -24,6 +25,39 @@ class Analogy:
                 input_op = tf.constant(self._entries, name="analogy_entries")
                 self._op = tf.gather(embeddings, input_op)
         return self._op
+
+    def calculate_accuracy(self, embeddings):
+        vectors = list()
+        results = list()
+        for entry in self._entries:
+            vectors.append(embeddings[entry[1]]-embeddings[entry[0]]+embeddings[entry[2]])
+            results.append(entry+[
+                np.linalg.norm(embeddings[entry[1]]-embeddings[entry[0]]),
+                np.linalg.norm(embeddings[entry[3]]-embeddings[entry[2]]),
+            ])
+
+        true = 0
+        processed = 0
+        total = len(vectors)
+        timestamp = time.time()
+        for idx in range(total):
+            if time.time() - timestamp > 200:
+                print("Analogy evaluation: processed", processed, "out of", total,
+                      ", current accuracy:", float(true) / float(total))
+                timestamp = time.time()
+            distances = np.squeeze(cdist(embeddings, [vectors[idx]]))
+            result = np.argsort(distances)
+            ethalon = distances[result[0]]
+            for index in result:
+                if abs(ethalon - distances[index]) > 0.01:
+                    break
+                results[idx].append(index)
+                if index == self._entries[idx][3]:
+                    true += 1
+                    break
+            processed += 1
+
+        return float(true) / float(total), results
 
     def calculate_analogy(self, session, embeddings_size=None):
         if self._op is None:
@@ -191,12 +225,43 @@ class EvaluationDumper(Thread):
         del self._evaluation
 
 
+class EmbeddingsDumper(Thread):
+    def __init__(self, embeddings, tensor, dictionary, save_to=''):
+        Thread.__init__(self)
+        self._embeddings = embeddings
+        self._tensor = tensor
+        self._output = save_to
+        self._dictionary = dictionary
+
+    def run(self):
+        self._embeddings.dump(
+            self._output,
+            self._tensor,
+            self._dictionary
+        )
+
+
+class AnalogyCalculation(Thread):
+    def __init__(self, analogy, tensor, log):
+        Thread.__init__(self)
+        self._analogy = analogy
+        self._tensor = tensor
+        self._log = log
+        self.results = None
+
+    def run(self):
+        analogy_time = time.time()
+        accuracy, self.results = self._analogy.calculate_accuracy(self._tensor)
+        self._log.print("Analogy accuracy:", "%.2f" % accuracy)
+        self._log.print("Done in", "%.2f" % (time.time() - analogy_time), "seconds")
+
+
 def dump_evaluation(evaluation, postfix, folder=''):
     dumper = EvaluationDumper(evaluation, postfix, folder=folder)
     dumper.start()
     return dumper
 
 
-def control_evaluation(threads):
+def control_tasks(threads):
     for dumper in threads:
         dumper.join()

bk2vec_categories.py
@@ -205,10 +205,10 @@ with graph.as_default():
     # Construct the SGD optimizer using a learning rate of 1.0.
     loss_summary = tf.scalar_summary("joint_loss", joint_loss)
     learning_rate = 1.0
-    #learning_rate = tf.train.exponential_decay(1.0, global_step,
-    #                                           150000, 0.9, staircase=True)
+    #learning_rate = tf.train.exponential_decay(1.0, global_step, 150000, 0.9, staircase=True)
     optimizer = tf.train.AdagradOptimizer(learning_rate, use_locking=True, name="joint_objective").minimize(joint_loss, global_step=global_step)
     merged = tf.merge_all_summaries()
+    embeddings.normalized_tensor()
 
 
 class TrainingWorker(Thread):
@@ -253,6 +253,7 @@ class TrainingWorker(Thread):
                 average_cats = 0
 
 # Step 5: Begin training.
+tasks = []
 with tf.Session(graph=graph) as session:
     timestamp = time.time()
     writer = tf.train.SummaryWriter("logs", graph)
@@ -298,8 +299,8 @@ with tf.Session(graph=graph) as session:
                 print("Relations (showing %d out of %d):" % (num, total))
                 for idx in range(num):
                     debug_example = [dictionary.rev_dict[ele] for ele in relations_debug[idx]]
-                    print(debug_example[0], '-('+debug_example[1]+')>', debug_example[2], "(Corrupted:",
-                          debug_example[3], '-(' + debug_example[1] + ')>', debug_example[4]+')')
+                    print(debug_example[0], '-(' + debug_example[1] + ')>', debug_example[2], "(Corrupted:",
+                          debug_example[3], '-(' + debug_example[1] + ')>', debug_example[4] + ')')
             print("Batch resolved: ", " ".join([dictionary.rev_dict[ele] for ele in batch[-20:]]))
             #print("Batch: ", batch.shape, batch[-20])
             #print("Examples: ", examples.shape, examples[-50:])
@@ -333,6 +334,35 @@ with tf.Session(graph=graph) as session:
                       "%.2f" % analogy_time,
                       "("+str(int(timeadj))+"s total)")
 
+        if show % 200 == 0:
+            log.print("Waiting for old tasks to complete...")
+            alive_tasks = []
+            for task in tasks:
+                if task.is_alive():
+                    alive_tasks.append(task)
+            if len(alive_tasks) == 0:
+                tasks = []
+                timestamp = time.time()
+                norm_tensor = embeddings.normalized_tensor().eval()
+                log.print("Tensor normalized in", ("%.5f" % (time.time() - timestamp)), "s")
+                log.print("Dumping embeddings")
+                output_file = args.output + 'embeddings-prov-' + str(embeddings.size) + '-' + get_num_stems_str(args.iterations) + '.tsv'
+                dump_task = EmbeddingsDumper(
+                    embeddings,
+                    norm_tensor,
+                    dictionary,
+                    save_to=output_file
+                )
+                dump_task.start()
+                analogy_task = AnalogyCalculation(analogy, norm_tensor, log)
+                analogy_task.start()
+                tasks.append(dump_task)
+                tasks.append(analogy_task)
+                del norm_tensor
+            else:
+                log.print("Skipping analogy calculation and embeddings dumping because previous run hasn't completed yet")
+                tasks = alive_tasks
+
     coord.request_stop()
     coord.join(threads)
 
@@ -344,11 +374,15 @@ with tf.Session(graph=graph) as session:
 
 # Step 6: Dump embeddings to file
 log.print("Dumping embeddings on disk")
-embeddings.dump(
-    args.output+'embeddings-' + str(embeddings.size) + '-' + get_num_stems_str(args.iterations) + '.tsv',
+control_tasks(tasks)
+dump_task = EmbeddingsDumper(
+    embeddings,
     norm_tensor,
-    dictionary
+    dictionary,
+    args.output+'embeddings-' + str(embeddings.size) + '-' + get_num_stems_str(args.iterations) + '.tsv'
 )
+dump_task.start()
+dump_task.join()
 
 #log.print("Waiting for evaluation dumping to finish...")
 #control_evaluation(tasks)

evaluation.py
@@ -1,111 +1,23 @@
-# coding=utf-8
 from __future__ import absolute_import
 from __future__ import print_function
 
-import gzip
-import csv
-import tensorflow as tf
-import numpy as np
-import os.path
-
 from bk2vec.embeddings import Embeddings
+from bk2vec.evaluation import *
+from bk2vec.utils import *
 from bk2vec.arguments import EvaluationArguments
 
 args = EvaluationArguments().show_args().args
 
-
-def restore_evaluation(filename):
-    pages = dict()
-    categories = 0
-    unique_categories = dict()
-    with gzip.open(filename, 'rb') as csvfile:
-        reader = csv.reader(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE, quotechar='')
-        for row in reader:
-            if len(row) == 0:
-                continue
-            index = int(row[0])
-            pages[index] = np.array(map(int, row[1:]))
-            for category in row[1:]:
-                unique_categories[category] = 0
-            categories += len(pages[index])
-    print("Loaded", len(pages.values()), "pages with", categories, "categories", len(unique_categories), "unique categories")
-    return pages
-
-
-if not os.path.exists(args.test):
-    print("Categories file doesn't exist: " + args.test)
-    exit()
-if not os.path.exists(args.embeddings):
-    print("Embeddings file doesn't exist: " + args.embeddings)
-    exit()
-print("Restoring test set")
-pages = restore_evaluation(args.test)
 print("Restoring embeddings")
-embeddings, rev_dict, _ = Embeddings.restore(args.embeddings)
+embeddings, dictionary = Embeddings.restore(args.embeddings)
 print("Restored embeddings with shape:", embeddings.shape)
+relations = Analogy.from_file("datasets/questions-words.txt", dictionary.dict)
 
-graph = tf.Graph()
-
-
-def matrix_distance(tensor1, tensor2):
-    with tf.name_scope("matrix_distance"):
-        sub = tf.sub(tensor1, tensor2)
-        distance = tf.sqrt(tf.clip_by_value(tf.reduce_sum(tf.square(sub), 1), 1e-10, 1e+37))
-        return distance
-
-log = open(args.test+'.log', 'wb')
-log.write(str(vars(args)))
-log.write('\n')
-def print_log(*args):
-    print(*args)
-    log.write(" ".join([str(ele) for ele in args]))
-    log.write('\n')
-
-with graph.as_default():
-    # Input
-
-    # Look up embeddings for inputs.
-    category_tensor = tf.placeholder(tf.float32, shape=[None, embeddings.shape[1]])
-    page_tensor = tf.placeholder(tf.float32, shape=[None, embeddings.shape[1]])
-
-    category_loss = tf.reduce_mean(matrix_distance(
-        category_tensor,
-        page_tensor
-    ))
-
-    # Graph
-    loss = 0.0
-    counts = 0
-    print("Starting calculating pages loss")
-    indices = list()
-    page_indices = list()
-    last_indices = list()
-    last_page_indices = list()
-    for page in pages.keys():
-        if len(pages[page]) == 0:
-            continue
-        counts += 1
-        for ele in pages[page]:
-            indices.append(ele)
-            page_indices.append(page)
-        if counts % 100 == 0:
-            category_input = np.take(embeddings, indices, axis=0)
-            page_input = np.take(embeddings, page_indices, axis=0)
-            last_indices = indices
-            last_page_indices = page_indices
-            indices = list()
-            page_indices = list()
-            with tf.Session(graph=graph) as session:
-                loss += session.run(category_loss, feed_dict={category_tensor: category_input, page_tensor: page_input})
-        if counts % 1000000 == 0:
-            print_log("Last input shape:", category_input.shape, page_input.shape)
-            print_log("Last amount of categories:", len(last_page_indices))
-            print_log("Last pages -> categories:")
-            for idx in range(20):
-                print(rev_dict[last_page_indices[idx]], '->', rev_dict[last_indices[idx]])
-            print_log("  " + str(counts // 1000) + "k pages parsed")
-            print_log("  Avg loss:", loss / (counts / 100))
-
-    print_log("Average loss: ", loss / (counts / 100))
-
-log.close()
+thread = AnalogyCalculation(relations, embeddings, Log(args))
+thread.start()
+thread.join()
+for result in thread.results[:100]:
+    print("")
+    print("Sample: ", [dictionary.rev_dict[ele] for ele in result[:4]])
+    print("Candidates: ", [dictionary.rev_dict[ele] for ele in result[6:]])
+    print("Relation norms: ", "%.2f" % result[4], "%.2f" % result[5])